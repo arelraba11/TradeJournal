73c5d9dd1bd7f38cf7787a85427e578c2f242946
Arel Raba <arelraba11@gmail.com>
2025-04-12 14:14:27 +0300

    Initial commit: backend project structure

--- backend/src/main/java/com/arelraba/tradejournal/backend/BackendApplication.java
+++ backend/src/main/java/com/arelraba/tradejournal/backend/BackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TradejournalBackendApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TradejournalBackendApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
